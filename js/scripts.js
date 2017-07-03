6e1ea8db949bf35147cfa643ca3a4fad2abf8ce8
paulo maia <pmanzonemaia@gmail.com>
2017-07-03 19:58:44 -0300

    Mudanças index principal

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 		var id = "T" + (i+2);
 
 		console.log(id);
-		$("#trabalhos").append('<div><a href="' + id + '/index.html"><img src=' + id+ '/imgs/1.png></a></div>"');
+		$("#trabalhos").append('<div class="col-sm-3 box"><a href="' + id + '/index.html"><img src=' + id+ '/imgs/1.png></a></div>');
 	}
 })

dac98391b734616dbfd2273c53b7a2de8481b02d
Comini2 <joaocomini@gmail.com>
2017-07-26 04:17:45 -0300

    Indice adicionado ao index principal.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-	var nomes = ["Raízes", "Sistemas", "Interpolação", "Ajuste"];
+	var nomes = ["Raízes", "Sistemas", "Interpolação", "Ajuste", "Integrais"];
 
-	for(var i = 0; i<4; i++){
+	for(var i = 0; i<5; i++){
 		var id = "T" + (i+2);
 
 		$("#trabalhos").append('<div class="box" id ="'+ id +'"><a href="' + id + '/index.html"><img src=' + id+ '/imgs/1.png><p>' + nomes[i] + '</p></a></div>');
